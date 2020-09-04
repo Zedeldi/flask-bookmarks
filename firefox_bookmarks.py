5ec25b5c25b879fe130b82ff0d11131f27aa948d
Zedeldi <66186954+Zedeldi@users.noreply.github.com>
2020-09-04 12:38:24 +0100

    Export to HTML, check database integrity

--- firefox_bookmarks.py
+++ firefox_bookmarks.py
@@ -1,14 +1,45 @@
 import sqlite3, time
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, request, render_template
 from benedict import benedict
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
 DATABASE="places.sqlite"
+conn = sqlite3.connect(DATABASE)
+c = conn.cursor()
+if len(c.execute("""
+	SELECT name
+	FROM sqlite_master
+	WHERE type='table'
+	AND name='moz_bookmarks'
+	OR name='moz_places'
+""").fetchall()) != 2: # Check that moz_bookmarks and moz_places exist
+	print("Invalid database.")
+	exit(1)
+else: conn.close()
+
+def export_html(d, fd):
+	for key in d:
+		try: fd.write("""
+	<DT><A HREF="{0}" 
+	ADD_DATE="{1}" 
+	LAST_MODIFIED="{2}">
+	{3}
+	</A>
+		""".format(d[key]['url'], d[key]['date'], d[key]['modified'], d[key]['title']))
+		except KeyError: # d[key] is a folder
+			fd.write("""
+</DL><p>
+<DT><H3>{0}</H3>
+<DL><p>
+			""".format(key))
+			export_html(d[key], fd) # Recursively parse children
 
 @app.route('/')
 def get_bookmarks():
+	use_html = request.args.get('html', default=False)
+	
 	conn = sqlite3.connect(DATABASE)
 	conn.row_factory = sqlite3.Row # Get dictionary-like objects for readability
 	c = conn.cursor()
@@ -35,7 +66,7 @@ def get_bookmarks():
 		bookmarks[tree]={} # Create empty dictionary for bookmarks
 		parent=(folder['id'],)
 		for data in c.execute("""
-			SELECT moz_places.id, moz_bookmarks.title, moz_places.url, moz_bookmarks.dateAdded
+			SELECT moz_places.id, moz_bookmarks.title, moz_places.url, moz_bookmarks.dateAdded, moz_bookmarks.lastModified
 			FROM moz_places
 			JOIN moz_bookmarks
 			ON moz_places.id = moz_bookmarks.fk
@@ -44,12 +75,21 @@ def get_bookmarks():
 			bookmarks[tree][data['id']]={
 				"title": data['title'],
 				"url": data['url'],
-				"date": time.strftime('%a, %d-%m-%Y %T', time.localtime(data['dateAdded']/1000000))
+				"date": data['dateAdded']/1000000,
+				"modified": data['lastModified']/1000000
 				} # Add the bookmarks to their parent folder
-			# See also: datetime.fromtimestamp(data['dateAdded']/1000000)
-			# https://developer.mozilla.org/en-US/docs/Mozilla/Projects/NSPR/Reference/PRTime - Mozilla uses microseconds, hence divide by 10^6
+			# See also: time.strftime('%a, %d-%m-%Y %T', time.localtime(data['dateAdded']/1000000)),
+			# and: datetime.fromtimestamp(data['dateAdded']/1000000) to convert Unix time
+			# https://developer.mozilla.org/en-US/docs/Mozilla/Projects/NSPR/Reference/PRTime - Mozilla uses microseconds resolution, hence divide by 10^6
+		
+	if use_html:
+		filename="bookmarks{0}.html".format(time.strftime('-%Y-%m-%d', time.localtime()))
+		with open(filename, 'w+') as fd: # Create file descriptor outside of recursive function
+			export_html(bookmarks, fd) # Recursively parse JSON and write HTML to file
+			fd.seek(0)
+			return fd.read() # Return the HTML
 	
-	return bookmarks
+	return bookmarks # Else, return the JSON/dict
 
 @app.route('/add', methods=['POST', 'GET'])
 def add_bookmark():
@@ -83,7 +123,7 @@ def add_bookmark():
 			
 			title=request.form['title']
 			
-			date=time.time()*1000000
+			date=time.time()*1000000 # Omit the decimal point
 
 			place=(fk, url, title)
 			c.execute("""
